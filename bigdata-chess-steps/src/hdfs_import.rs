b283f9af9d6755b9f918ec6ed9fa94347fc96d83
Nikita Volobuev <nikitavbv@gmail.com>
2023-01-07 18:17:53 +0200

    feat(hdfs_import): hdfs import step is ready

--- bigdata-chess-steps/src/hdfs_import.rs
+++ bigdata-chess-steps/src/hdfs_import.rs
@@ -1,8 +1,3 @@
-// TODO: implement agent which downloads file from storage
-// after downloading the file, it is uploaded to hdfs
-// one both steps are ready, implement some way to tell which files are
-// already synced.
-
 use {
     std::{sync::Arc, time::Duration, collections::HashSet},
     tracing::info,
@@ -14,10 +9,10 @@ pub async fn hdfs_import_step(storage: Arc<Storage>) {
     info!("running hdfs import step");
 
     loop {
-        let games_in_remote_storage = storage.remote_list_game_data_files().await.unwrap();
         let keys_in_local_storage = load_sync_state().await;
         let mut synced_keys = HashSet::new();
 
+        let games_in_remote_storage = storage.remote_list_game_data_files().await.unwrap();
         for game_in_remote_storage in games_in_remote_storage {
             if !keys_in_local_storage.contains(&game_in_remote_storage) {
                 synced_keys.insert(game_in_remote_storage.clone());
@@ -27,22 +22,59 @@ pub async fn hdfs_import_step(storage: Arc<Storage>) {
 
                 info!("syncing game {}", file_id);
                 let game = storage.remote_game_data_file(&game_in_remote_storage).await.unwrap();
-                fs::write(format!("{}.csv", local_file_path), game).await.unwrap();
+                fs::write(&local_file_path, game).await.unwrap();
 
                 info!("uploading game into hdfs: {}", file_id);
-                let mut child = Command::new("hadoop").arg("fs").arg("-put").arg("/tables_data/chess_games").spawn().unwrap();
-                let status = child.wait().await.unwrap();
-                info!("upload into hdfs finished with status: {}", status);
-
-                fs::remove_file(format!("{}.csv", local_file_path)).await.unwrap();
+                hdfs_put(&local_file_path, "chess_games").await;
+                fs::remove_file(local_file_path).await.unwrap();
+            
+                let mut all_keys = keys_in_local_storage.clone();
+                all_keys.extend(synced_keys.clone().into_iter());
+                save_sync_state(&all_keys).await;
+            } else {
+                info!("game already synced: {}", game_in_remote_storage);
             }
         }
 
-        // TODO: sync game moves
+        let game_moves_in_remote_storage = storage.remote_list_game_moves_files().await.unwrap();
+        for game_move_in_remote_storage in game_moves_in_remote_storage {
+            if !keys_in_local_storage.contains(&game_move_in_remote_storage) {
+                synced_keys.insert(game_move_in_remote_storage.clone());
+
+                let file_id = file_name_from_path(&game_move_in_remote_storage);
+                let local_file_path = format!("./{}.csv", file_id);
+
+                info!("syncing game {}", file_id);
+                let game = storage.remote_game_data_file(&game_move_in_remote_storage).await.unwrap();
+                fs::write(&local_file_path, game).await.unwrap();
+
+                info!("uploading game moves into hdfs: {}", file_id);
+                hdfs_put(&local_file_path, "chess_game_moves").await;
+                fs::remove_file(local_file_path).await.unwrap();
+
+                let mut all_keys = keys_in_local_storage.clone();
+                all_keys.extend(synced_keys.clone().into_iter());
+                save_sync_state(&all_keys).await;
+            } else {
+                info!("game moves already synced: {}", game_move_in_remote_storage);
+            }
+        }
 
         info!("sleeping before the next iteration");
         sleep(Duration::from_secs(60 * 60)).await;
     }
+}
+
+async fn hdfs_put(local_file_path: &str, table_name: &str) {
+    let mut child = Command::new("hadoop")
+        .arg("fs")
+        .arg("-put")
+        .arg(local_file_path)
+        .arg(format!("/tables_data/{}", table_name))
+        .spawn()
+        .unwrap();
+    let status = child.wait().await.unwrap();
+    info!("upload into hdfs finished with status: {}", status);
 }
 
 fn file_name_from_path(path: &str) -> String {
@@ -55,4 +87,8 @@ async fn load_sync_state() -> HashSet<String> {
         .ok()
         .and_then(|v| serde_json::from_slice(&v).unwrap())
         .unwrap_or(HashSet::new())
+}
+
+async fn save_sync_state(state: &HashSet<String>) {
+    fs::write("chess_hdfs_sync_state.json", serde_json::to_vec(state).unwrap()).await.unwrap();
 }
