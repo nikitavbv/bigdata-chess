dc0737e8ce21dd43e615ab2c3a4078809211135c
Nikita Volobuev <nikitavbv@gmail.com>
2023-01-10 22:52:56 +0200

    feat(chunk-splitter): try to fix block_on future

--- bigdata-chess-steps/src/chunk_splitter.rs
+++ bigdata-chess-steps/src/chunk_splitter.rs
@@ -189,7 +189,7 @@ pub async fn chunk_splitter_step(config: &ChunkSplitterStepConfig, storage: Arc<
 }
 
 pub struct LichessDataFileChunkReader {
-    rt: tokio::runtime::Runtime,
+    handle: tokio::runtime::Handle,
 
     storage: Arc<Storage>,
     path: String,
@@ -202,7 +202,7 @@ pub struct LichessDataFileChunkReader {
 impl LichessDataFileChunkReader {
     pub fn new(storage: Arc<Storage>, path: String, total_chunks: u64) -> Self {
         Self {
-            rt: tokio::runtime::Runtime::new().unwrap(),
+            handle: tokio::runtime::Handle::current(),
             storage,
             path,
             total_chunks,
@@ -216,7 +216,8 @@ impl Read for LichessDataFileChunkReader {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         while buf.len() > self.chunk_buffer.len() && self.current_chunk.unwrap_or(0) < self.total_chunks {
             let chunk_to_fetch = self.current_chunk.unwrap_or(0);
-            let mut chunk_data = self.rt.block_on(async {
+            let _ = self.handle.enter();
+            let mut chunk_data = futures::executor::block_on(async {
                 info!("reading next chunk: {}", chunk_to_fetch);
 
                 let res = match self.storage.get_lichess_data_file_chunk(&self.path, chunk_to_fetch).await {
